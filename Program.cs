a9ddb211562ac6cfdf10f91f4d03622e81259739
Soomin Song <song.soomin.418@outlook.com>
2025-05-05 12:43:07 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// 1. Given an integer x, return true if x is a palindrome, and false otherwise.
+//Example 1:
+//Input: x = 121
+//Output: true
+//Explanation: 121 reads as 121 from left to right and from right to left.
+//Example 2:
+//Input: x = -121
+//Output: false
+//Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+
+
+//2. Write a program in C# Sharp to create a function to calculate the sum of the individual digits of a given number.
+//Test Data :
+//Enter a number: 1234
+//Expected Output :
+//The sum of the digits of the number 1234 is : 10
+
+
+
+//3.Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+//Example 1:
+//Input: nums = [1, 2, 3, 1]
+//Output: true
+//Example 2:
+//Input: nums = [1, 2, 3, 4]
+//Output: false
+//Example 3:
+//Input: nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+//Output: true
+
+
